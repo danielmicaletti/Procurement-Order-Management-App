c0629e8ba1e81ffac1d0bb3f13a504e46e12018a
Daniel Micaletti <danielmicaletti@gmail.com>
2015-05-04 17:19:38 +0300

    static settings

--- wease/settings.py
+++ wease/settings.py
@@ -108,11 +108,11 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 
 STATICFILES_DIRS = (
 
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
     os.path.join(BASE_DIR, 'media'),
 )
 
